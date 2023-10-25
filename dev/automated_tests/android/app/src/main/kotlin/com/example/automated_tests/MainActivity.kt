147aacb8f9f19179de2daada6925537260e6f6d8
JDTyler <jdtyler.dev@outlook.com>
2023-10-25 11:49:11 -0500

    Revert "."
    This reverts commit db84579332d979ee609aa6218209389054bbdcf4.

---
+++ dev/automated_tests/android/app/src/main/kotlin/com/example/automated_tests/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.automated_tests
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ service_matrix/android/app/src/main/kotlin/com/example/service_matrix/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.service_matrix
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
